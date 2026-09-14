425715b0442c36f2cc826629bee59bc2675103ec
Vishnuvardhan K <vishtechie07@gmail.com>
2026-09-14 17:03:27 +1200

    refactor: reduce duplication in item checks and quality handling.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -14,54 +14,78 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.name.equals(AGED_BRIE)
-                    && !item.name.equals(BACKSTAGE_PASS)) {
-                if (item.quality > 0) {
-                    if (!item.name.equals(SULFURAS)) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
 
-                    if (item.name.equals(BACKSTAGE_PASS)) {
+            if (!isAgedBrie(item)
+                    && !isBackstagePass(item)) {
+
+                if (!isSulfuras(item)) {
+                    decreaseQuality(item);
+                }
+
+            } else {
+
+                if (item.quality < 50) {
+                    increaseQuality(item);
+
+                    if (isBackstagePass(item)) {
+
                         if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
+                            increaseQuality(item);
                         }
 
                         if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
+                            increaseQuality(item);
                         }
                     }
                 }
             }
 
-            if (!item.name.equals(SULFURAS)) {
+            if (!isSulfuras(item)) {
                 item.sellIn = item.sellIn - 1;
             }
 
             if (item.sellIn < 0) {
-                if (!item.name.equals(AGED_BRIE)) {
-                    if (!item.name.equals(BACKSTAGE_PASS)) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals(SULFURAS)) {
-                                item.quality = item.quality - 1;
-                            }
+
+                if (!isAgedBrie(item)) {
+
+                    if (!isBackstagePass(item)) {
+
+                        if (!isSulfuras(item)) {
+                            decreaseQuality(item);
                         }
+
                     } else {
                         item.quality = item.quality - item.quality;
                     }
+
                 } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
+                    increaseQuality(item);
                 }
             }
+        }
+    }
+
+    private boolean isSulfuras(Item item) {
+        return SULFURAS.equals(item.name);
+    }
+
+    private boolean isAgedBrie(Item item) {
+        return AGED_BRIE.equals(item.name);
+    }
+
+    private boolean isBackstagePass(Item item) {
+        return BACKSTAGE_PASS.equals(item.name);
+    }
+
+    private void increaseQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality++;
+        }
+    }
+
+    private void decreaseQuality(Item item) {
+        if (item.quality > 0) {
+            item.quality--;
         }
     }
 }
